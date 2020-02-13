32b71f28ca949112d81987e901dabf3aaab4455b
LSafer <lsaferse@gmail.com>
2020-02-13 13:28:37 +0300

    Create Do.java
    Do.java is a loop that runs the code just one time after been started.

---
+++ src/main/java/org/cufy/lang/Do.java
@@ -0,0 +1,36 @@
+package org.cufy.lang;
+
+import cufy.lang.Loop;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+/**
+ * A loop to do code just one time.
+ *
+ * @author LSaferSE
+ * @version 1 release (13-Feb-2020)
+ * @since 13-Feb-2020
+ */
+public class Do extends Loop<Consumer<Do>, Object> {
+	/**
+	 * Construct a new 'do' loop.
+	 */
+	public Do() {
+	}
+	/**
+	 * Construct a new 'do' loop with the given parameters.
+	 *
+	 * @param code the first looping code
+	 * @throws NullPointerException if the given code is null
+	 */
+	public Do(Consumer<Do> code) {
+		Objects.requireNonNull(code, "code");
+		this.append(code);
+	}
+
+	@Override
+	protected void loop() {
+		this.next(null);
+	}
+}
